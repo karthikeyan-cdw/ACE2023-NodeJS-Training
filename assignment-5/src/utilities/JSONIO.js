6f48bb92ea911e959a08baeb4bfaadb7be319bc5
karthikeyan-cdw <karthikeyan.chokalingam@cdw.com>
2023-04-03 23:17:07 +0530

    Added Database url from env variables

--- assignment-5/src/utilities/JSONIO.js
+++ assignment-5/src/utilities/JSONIO.js
@@ -1,14 +1,11 @@
 // importing the required modules
 const fs = require("fs");
 
-// adding the database
-const database = "./database/cdw_ace23_buddies.json";
-
 const writeJSON = (input_data) => {
   try {
     const data = JSON.stringify(input_data);
     try {
-      fs.writeFileSync(database, data);
+      fs.writeFileSync(process.env.DATABASE_URL, data);
       return { status: 200, data: "Data Successfully written" };
     } catch {
       return { status: 500, data: "Unable to write to Database" };
@@ -19,7 +16,7 @@ const writeJSON = (input_data) => {
 };
 const readJSON = () => {
   try {
-    const data = fs.readFileSync(database, "utf-8");
+    const data = fs.readFileSync(process.env.DATABASE_URL, "utf-8");
     try {
       return { status: 200, data: JSON.parse(data) };
     } catch {
