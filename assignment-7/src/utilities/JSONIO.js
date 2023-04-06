14f53def4c0e471aa6c275ad2a1f12470938a52a
karthikeyan-cdw <karthikeyan.chokalingam@cdw.com>
2023-04-06 08:53:01 +0530

    Generalized IO Functions

--- assignment-7/src/utilities/JSONIO.js
+++ assignment-7/src/utilities/JSONIO.js
@@ -4,19 +4,21 @@ const fs = require("fs");
 /**
  * @summary The function writes input data to a JSON file and returns a status and message indicating success or
  * failure.
- * @param input_data - The input_data parameter is the data that needs to be written to a JSON file. It
- * should be in a format that can be converted to a JSON string using the JSON.stringify() method.
+ * @param database - The database parameter is a string that represents the file path of the JSON file
+ * where the data will be written.
+ * @param input_data - The input_data parameter is the data that needs to be written to the database in
+ * JSON format.
  * @returns The function `writeJSON` returns an object with a `status` property and a `data` property.
- * The `status` property indicates the status of the operation (either 200 for success or 500 for
- * failure), and the `data` property contains a message indicating the result of the operation. If
- * there is an error, the object also contains an `error` property with a string describing it.
+ * If the data is successfully written to the database, the `status` property is set to 200 and the
+ * `data` property is set to "Data Successfully written". If there is an error writing to the database,
+ * the `status` property is set to 500.
  * @author @karthikeyan-cdw
  */
-const writeJSON = (input_data) => {
+const writeJSON = (database, input_data) => {
   try {
     const data = JSON.stringify(input_data);
     try {
-      fs.writeFileSync(process.env.DATABASE_URL, data);
+      fs.writeFileSync(database, data);
       return { status: 200, data: "Data Successfully written" };
     } catch (error) {
       return {
@@ -35,17 +37,19 @@ const writeJSON = (input_data) => {
 };
 
 /**
- * @summary This function reads a JSON file from a specified database URL and returns either the parsed data or
- * an error message.
+ * @summary The function reads a JSON file from a database and returns either the parsed data or an error
+ * message.
+ * @param database - The `database` parameter is a string representing the file path of the JSON
+ * database that needs to be read.
  * @returns The function `readJSON` returns an object with a `status` property and a `data` property.
- * If the `fs.readFileSync` method is successful in reading the file specified in the
- * `process.env.DATABASE_URL` environment variable, the `data` property will contain the parsed JSON
- * data from the file and the `status` property will be set to 200. If there is an error, returns `404`.
+ * The `status` property indicates the status of the operation (200 for success, 500 for error), and
+ * the `data` property contains the result of the operation (either the parsed JSON data or an error
+ * message). If an error occurs, the object also includes an `error` property.
  * @author @karthikeyan-cdw
  */
-const readJSON = () => {
+const readJSON = (database) => {
   try {
-    const data = fs.readFileSync(process.env.DATABASE_URL, "utf-8");
+    const data = fs.readFileSync(database, "utf-8");
     try {
       return { status: 200, data: JSON.parse(data) };
     } catch (error) {
